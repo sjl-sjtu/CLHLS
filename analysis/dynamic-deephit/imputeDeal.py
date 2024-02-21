d91e192c02b878b26247a9412e1d4770451d927a
Sun Jianle <53631578+sjl-sjtu@users.noreply.github.com>
2024-02-21 22:07:10 +0800

    Update imputeDeal.py

--- analysis/dynamic-deephit/imputeDeal.py
+++ analysis/dynamic-deephit/imputeDeal.py
@@ -28,6 +28,9 @@ IDin = np.unique(IDin)
 IDs = np.setdiff1d(IDs,IDin)
 df = df.loc[df["id"].isin(IDs),:]
 
+# 删除发病后的记录
+df = df.drop(df[(df["timenow"]>df["totaltime"]) & (df["CHD"]==1)].index)
+
 #df = df.dropna(axis = 0, subset = ['times'] )
 df = df.dropna(axis = 0, how = 'any', subset = ['times','ADL','bmi','emo'])
 
@@ -49,48 +52,48 @@ df.to_csv("dflongi_use2.csv",index=False)
 
 
 
-vlist = []
-for i in list(range(1,22)):
-    v = "V"+str(i)
-    vlist.append(v)
+# vlist = []
+# for i in list(range(1,22)):
+#     v = "V"+str(i)
+#     vlist.append(v)
 
-IDs = df.loc[:,"ID"]
-IDs = np.unique(IDs)
-IDs = np.sort(IDs)
+# IDs = df.loc[:,"ID"]
+# IDs = np.unique(IDs)
+# IDs = np.sort(IDs)
 
-df1 = df.loc[df["CAS"]==1,:]
-df0 = df.loc[df["CAS"]==0,:]
+# df1 = df.loc[df["CAS"]==1,:]
+# df0 = df.loc[df["CAS"]==0,:]
 
-#去极端值：箱线图
-def barboxTest(df):
-    xID= []
-    for i in vlist:
-        lower_q=np.quantile(df[i],0.25,interpolation='lower')#下四分位数
-        higher_q=np.quantile(df[i],0.75,interpolation='higher')#上四分位数
-        int_r=higher_q-lower_q#四分位
-        U = higher_q + 1.5*int_r
-        L = lower_q - 1.5*int_r
-        xID.append(df.loc[df[ df[i]<L].index,"ID"].tolist())
-        xID.append(df.loc[df[ df[i]>U].index,"ID"].tolist())
-        df.loc[df[df[i]<L].index,i] = np.nan
-        df.loc[df[df[i]>U].index,i] = np.nan
-    return df,xID
-df0,xID0 = barboxTest(df0)
-df1,xID1 = barboxTest(df1)
-df = pd.concat([df1,df0])
-df = df.sort_values(["ID","year"],ascending=True)
-df.index = np.arange(0,df.shape[0],1)
+# #去极端值：箱线图
+# def barboxTest(df):
+#     xID= []
+#     for i in vlist:
+#         lower_q=np.quantile(df[i],0.25,interpolation='lower')#下四分位数
+#         higher_q=np.quantile(df[i],0.75,interpolation='higher')#上四分位数
+#         int_r=higher_q-lower_q#四分位
+#         U = higher_q + 1.5*int_r
+#         L = lower_q - 1.5*int_r
+#         xID.append(df.loc[df[ df[i]<L].index,"ID"].tolist())
+#         xID.append(df.loc[df[ df[i]>U].index,"ID"].tolist())
+#         df.loc[df[df[i]<L].index,i] = np.nan
+#         df.loc[df[df[i]>U].index,i] = np.nan
+#     return df,xID
+# df0,xID0 = barboxTest(df0)
+# df1,xID1 = barboxTest(df1)
+# df = pd.concat([df1,df0])
+# df = df.sort_values(["ID","year"],ascending=True)
+# df.index = np.arange(0,df.shape[0],1)
 
-#入组即为1
-# =============================================================================
+# # 入组即为1
+# # =============================================================================
 # IDin = df.loc[ (df["totaltime"]==0) & (df["CHD"]==1),"ID"]
 # IDs = [i for i in IDs if i not in IDin.tolist()]
 # df = df.loc[df["ID"].isin(IDs),:]
-# 
+
 # =============================================================================
-#删除发病后的记录
+# 删除发病后的记录
 # =============================================================================
-# df = df.drop(df[(df["timenow"]>df["totaltime"]) & (df["CHD"]==1)].index)
+df = df.drop(df[(df["timenow"]>df["totaltime"]) & (df["CHD"]==1)].index)
 # =============================================================================
 
 #控制变量缺失率
@@ -101,7 +104,7 @@ miss_rate_X = X.isnull().sum().sort_values(ascending=False) / X.shape[0]
 unmiss_feature = miss_rate_X[miss_rate_X<=1]
 unmiss_feature = unmiss_feature._stat_axis.values.tolist() 
 
-#控制个体
+# 控制个体
 # =============================================================================
 # miss_rate_ID = df.loc[:,unmiss_feature].isnull().sum(axis=1)/len(unmiss_feature)
 # plt.hist(miss_rate_ID,bins=[i*0.05 for i in list(range(0,22,1))])
@@ -118,33 +121,33 @@ unmiss_feature = unmiss_feature._stat_axis.values.tolist()
 # df.index = np.arange(0,df.shape[0],1)
 # =============================================================================
 
-grouped = df.groupby("ID")
-idnew = df["ID"].unique()
-id_unmiss_times = []
-for i in range(len(idnew)):
-    dfid = grouped.get_group(idnew[i])
-    id_unmiss_times.append(dfid.shape[0])
-df_idrate = pd.DataFrame({"ID":idnew,"unmiss_times":id_unmiss_times})
-unmiss_id = df_idrate.loc[df_idrate["unmiss_times"]>=1,"ID"]
-df = df.loc[df["ID"].isin(unmiss_id),:]
-#df = df.loc[df["frequency"]>=2]
+# grouped = df.groupby("ID")
+# idnew = df["ID"].unique()
+# id_unmiss_times = []
+# for i in range(len(idnew)):
+#     dfid = grouped.get_group(idnew[i])
+#     id_unmiss_times.append(dfid.shape[0])
+# df_idrate = pd.DataFrame({"ID":idnew,"unmiss_times":id_unmiss_times})
+# unmiss_id = df_idrate.loc[df_idrate["unmiss_times"]>=1,"ID"]
+# df = df.loc[df["ID"].isin(unmiss_id),:]
+# #df = df.loc[df["frequency"]>=2]
 
-#impute
-df["normtime"] = df["timenow"]
-normfeat = unmiss_feature+["age","normtime"]
-impfeat = unmiss_feature+["sex","age","CAS","normtime"]
-df.loc[:,normfeat] = (df.loc[:,normfeat] - df.loc[:,normfeat].min())/(df.loc[:,normfeat].max() - df.loc[:,normfeat].min())
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer,KNNImputer
-imp = KNNImputer(n_neighbors=10)
-imp = imp.fit(df.loc[:,impfeat])
-Ximp = imp.transform(df.loc[:,impfeat])
-Ximp = pd.DataFrame(Ximp)
-Ximp.columns = impfeat
-df.update(Ximp,overwrite=False)
-df = df.sort_values(["ID","year"],ascending=True)
-df.index = np.arange(0,df.shape[0],1)
-df["totaltime"] = df["time"]
+# #impute
+# df["normtime"] = df["timenow"]
+# normfeat = unmiss_feature+["age","normtime"]
+# impfeat = unmiss_feature+["sex","age","CAS","normtime"]
+# df.loc[:,normfeat] = (df.loc[:,normfeat] - df.loc[:,normfeat].min())/(df.loc[:,normfeat].max() - df.loc[:,normfeat].min())
+# from sklearn.experimental import enable_iterative_imputer
+# from sklearn.impute import IterativeImputer,KNNImputer
+# imp = KNNImputer(n_neighbors=10)
+# imp = imp.fit(df.loc[:,impfeat])
+# Ximp = imp.transform(df.loc[:,impfeat])
+# Ximp = pd.DataFrame(Ximp)
+# Ximp.columns = impfeat
+# df.update(Ximp,overwrite=False)
+# df = df.sort_values(["ID","year"],ascending=True)
+# df.index = np.arange(0,df.shape[0],1)
+# df["totaltime"] = df["time"]
 
-df.to_csv("dflongi_use2.csv",index=False)
+# df.to_csv("dflongi_use2.csv",index=False)
 
